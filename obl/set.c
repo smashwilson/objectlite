0e2a4259f30ab0f4c8528cc87e9f80d5cae0c323
Ash Wilson <smashwilson@gmail.com>
2009-10-24 18:21:46 -0400

    Started to work on the red-black tree set implementation.  Will continue on non-laptop.

---
+++ obl/set.c
@@ -0,0 +1,9 @@
+/**
+ * Copyright (C) 2009 Ashley J. Wilson, Roger E. Ostrander
+ * This software is licensed as described in the file COPYING in the root
+ * directory of this distribution.
+ *
+ * 
+ */
+
+#include "set.h"
---
+++ obl/set.h
@@ -0,0 +1,21 @@
+/**
+ * Copyright (C) 2009 Ashley J. Wilson, Roger E. Ostrander
+ * This software is licensed as described in the file COPYING in the root
+ * directory of this distribution.
+ *
+ * 
+ */
+
+#ifndef SET_H
+#define SET_H
+
+/* defined in set.c */
+struct obl_rb_node;
+
+struct obl_set {
+
+    struct obl_rb_node *root;
+
+};
+
+#endif /* SET_H */
